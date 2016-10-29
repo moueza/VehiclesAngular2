126848572aa1f03583b8a78709c96a7c1986edc6
peter <mouezapeter@gmail.com>
2016-10-29 19:45:28 +0200

    VehiclesListComponent

--- src/app/vehicle-list/vehicle-list.component.ts
+++ src/app/vehicle-list/vehicle-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-vehicle-list',
-  templateUrl: './vehicle-list.component.html',
-  styleUrls: ['./vehicle-list.component.css']
-})
-export class VehicleListComponent implements OnInit {
 
-  constructor() { }
+@Component({   moduleId: module.id,
+      selector: 'app-vehicles-list',
+      templateUrl: 'vehicles-list.component.html',
+      styleUrls: ['vehicles-list.component.css'] })
 
+
+
+
+
+
+
+
+
+
+export class VehiclesListComponent implements OnInit {
+  constructor() {
+  }
   ngOnInit() {
   }
-
 }
